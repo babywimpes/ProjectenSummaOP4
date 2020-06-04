faae6301739880b700e9d1f5ab8b5d1b9fa2ab9f
MikeVH <mikevanherwijnen8@gmail.com>
2020-06-04 09:47:39 +0200

    added quick homepage

--- page/index.php
+++ page/index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        .top-buffer{
+            margin-top: 400px;
+        }
+    </style>
     <title>Homepage</title>
 </head>
+
 <body>
-    <?php
+    <div class="container top-buffer" >
 
-
-?>
-
+        <div class="row">
+            <div class="col text-center">
+                <a class="btn btn-primary btn-lg" href="./beeldhome.php">Beeld Sessies</a>
+                <a class="btn btn-primary btn-lg" href="./lessonshome.php">Lessons Learned</a>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
